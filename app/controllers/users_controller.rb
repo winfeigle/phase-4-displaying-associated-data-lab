1eb07ee340d9a0907266631f1dfe1d02fa69a527
Win Feigle <win.feigle@gmail.com>
2023-01-04 06:14:56 -0700

    commit tree 172d89ed620845d9b68afdb089e4240fa55969e3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
 
     def show
         user = User.find(params[:id])
-        render json: user
+        render json: user, include: :items
     end
 end
